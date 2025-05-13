38322ce6d4424315dc26b2a6f8c9b36714954d0b
Rabia <rabia.oweis@gmail.com>
2025-05-13 00:46:17 -0700

    extract values from database instead of hard-coding

--- src/main/java/team1/mealgenius/CalendarDisplay.java
+++ src/main/java/team1/mealgenius/CalendarDisplay.java
@@ -7,19 +7,24 @@ import javax.swing.JTable;
 import javax.swing.table.TableColumn;
 //import javax.swing.table.TableColumnModel;
 import java.awt.Dimension;
+import java.time.LocalDate;
+import static team1.mealgenius.DatabaseQuery.selectCalendarMeal;
 
 /**
  *
  * @author Rabia
  */
 public class CalendarDisplay { // same as App.java
+    
+    
     public static void main(String[] args) {
         JFrame frame = new JFrame("Simple Calendar");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        LocalDate dt = LocalDate.parse("2025-05-02");
 
         String[] columns = {"","Sunday", "Monday", "Tuesday", "Wednesday","Thursday","Friday","Saturday"};
         String[][] data = {
-            {"Breakfast", "Egg Sandwhich","Cereal", "French Toast", "Pancakes", "Power Smoothie","Omlette","Avocado Toast"},
+            {"Breakfast", DatabaseQuery.selectCalendarMeal(dt,1) ,"Cereal", "French Toast", "Pancakes", "Power Smoothie","Omlette","Avocado Toast"},
             {"Lunch", "Pasta", "Fried Rice", "Fish Taco", "Pizza","Burrito","Burger","Chicken Sandwhich"},
             {"Dinner","Biryani", "Chicken Pot Pie", "BBQ chicken", "Daal","Mixed Veggi Stew", "Lasagna","Chicken Soup"},
             {"Snack", "Dried Mango", "Strawberry bowl", "Jello","Nuts","Hummus","Cheese stick","Protein Bar"} 
